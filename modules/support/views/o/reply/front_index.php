f9f806affb8c107b6321dd3a075998ba896682ce
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-02-16 16:01:36 +0700

    add crud reply source

---
+++ modules/support/views/o/reply/front_index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Support Feedback Replies (support-feedback-reply)
+ * @var $this ReplyController
+ * @var $model SupportFeedbackReply
+ * @var $dataProvider CActiveDataProvider
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2017 Ommu Platform (ommu.co)
+ * @created date 16 February 2017, 16:00 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Support Feedback Replies',
+	);
+?>
+
+<?php $this->widget('application.components.system.FListView', array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+	'pager' => array(
+		'header' => '',
+	), 
+	'summaryText' => '',
+	'itemsCssClass' => 'items clearfix',
+	'pagerCssClass'=>'pager clearfix',
+)); ?>
