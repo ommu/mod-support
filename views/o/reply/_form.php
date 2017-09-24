dd3ee07af1b35c2019ff9b5955f310edb168f09a
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-09-24 19:40:24 +0700

    change redactor widget location

--- views/o/reply/_form.php
+++ views/o/reply/_form.php
@@ -46,7 +46,7 @@
 			<div class="desc">
 				<?php 
 				//echo $form->textArea($model,'reply_message',array('rows'=>6, 'cols'=>50, 'class'=>'span-11 smaller'));
-				$this->widget('application.extensions.imperavi.ImperaviRedactorWidget', array(
+				$this->widget('application.vendors.yiiext.imperavi-redactor-widget.ImperaviRedactorWidget', array(
 					'model'=>$model,
 					'attribute'=>reply_message,
 					// Redactor options
