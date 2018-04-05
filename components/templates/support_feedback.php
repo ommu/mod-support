252b01274358ef8af8ed1669de93cc474406e4e7
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-04-05 13:13:00 +0700

    drop all source

--- components/templates/support_feedback.php
+++
@@ -1,3 +0,0 @@
-Dear {displayname}
-<p><strong>Terimakasih telah menghubungi kontak kami.</strong></p>
-<p>Pertanyaan anda:<br/><strong><i>{subject}</i></strong>,<br/>{message} telah berhasil dikirim pada {creation_date}</p>
--- components/templates/support_feedback_reply.php
+++
@@ -1,6 +0,0 @@
-Dear {displayname}
-<p><strong>Terimakasih telah menghubungi kontak kami.</strong></p>
-<p>Berikut adalah jawaban atas pertanyaan/kirik/saran yang telah Anda ajukan pada tanggal <em>{creation_date}</em> dengan subyek <em>{subject}</em></p>
-<p><strong>Message</strong>:<br/>{message}</p>
-<strong>Balasan</strong>: 
-{reply_message}
