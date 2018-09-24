4644b002d7b83ee261555f69dd75e8847521a6f0
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-09-24 07:00:14 +0700

    add _detail

---
+++ views/o/subject/_detail.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Support Feedback Subjects (support-feedback-subject)
+ * @var $this SubjectController
+ * @var $model SupportFeedbackSubject
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 Ommu Platform (www.ommu.co)
+ * @created date 15 March 2018, 14:05 WIB
+ * @link https://github.com/ommu/mod-support
+ *
+ */
+?>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		array(
+			'name'=>'subject_id',
+			'value'=>$model->subject_id,
+		),
+		array(
+			'name'=>'publish',
+			'value'=>$this->quickAction(Yii::app()->controller->createUrl('publish', array('id'=>$model->subject_id)), $model->publish),
+			'type'=>'raw',
+		),
+		array(
+			'name'=>'parent_id',
+			'value'=>$model->parent_id != 0 ? $model->parent->title->message : '-',
+		),
+		array(
+			'name'=>'subject_name',
+			'value'=>$model->subject_name ? $model->title->message : '-',
+		),
+		array(
+			'name'=>'creation_date',
+			'value'=>!in_array($model->creation_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->creation_date) : '-',
+		),
+		array(
+			'name'=>'creation_search',
+			'value'=>$model->creation->displayname ? $model->creation->displayname : '-',
+		),
+		array(
+			'name'=>'modified_date',
+			'value'=>!in_array($model->modified_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->modified_date) : '-',
+		),
+		array(
+			'name'=>'modified_search',
+			'value'=>$model->modified->displayname ? $model->modified->displayname : '-',
+		),
+		array(
+			'name'=>'updated_date',
+			'value'=>!in_array($model->updated_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->updated_date) : '-',
+		),
+	),
+)); ?>
--- views/o/user/admin_view.php
+++ views/o/user/admin_view.php
@@ -20,48 +20,7 @@
 ?>
 
 <div class="dialog-content">
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		array(
-			'name'=>'id',
-			'value'=>$model->id,
-		),
-		array(
-			'name'=>'publish',
-			'value'=>$this->quickAction(Yii::app()->controller->createUrl('publish', array('id'=>$model->id)), $model->publish),
-			'type'=>'raw',
-		),
-		array(
-			'name'=>'subject_search',
-			'value'=>$model->feedback->subject_id ? $model->feedback->subject->title->message : '-',
-		),
-		array(
-			'name'=>'message_i',
-			'value'=>$model->feedback->message ? $model->feedback->message : '-',
-		),
-		array(
-			'name'=>'user_id',
-			'value'=>$model->user->displayname ? $model->user->displayname : '-',
-		),
-		array(
-			'name'=>'creation_date',
-			'value'=>!in_array($model->creation_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->creation_date) : '-',
-		),
-		array(
-			'name'=>'modified_date',
-			'value'=>!in_array($model->modified_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->modified_date) : '-',
-		),
-		array(
-			'name'=>'modified_search',
-			'value'=>$model->modified->displayname ? $model->modified->displayname : '-',
-		),
-		array(
-			'name'=>'updated_date',
-			'value'=>!in_array($model->updated_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->updated_date) : '-',
-		),
-	),
-)); ?>
+	<?php echo $this->renderPartial('_detail', array('model'=>$model)); ?>
 </div>
 <div class="dialog-submit">
 	<?php echo CHtml::button(Yii::t('phrase', 'Close'), array('id'=>'closed')); ?>
