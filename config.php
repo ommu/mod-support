0779ec1c64f4f1cd35bd44c9768c71fa74bfb5d8
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-01-02 17:10:56 +0700

    commit

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 return [
 	'id' => 'support',
-	'class' => app\modules\support\Module::className(),
+	'class' => ommu\support\Module::className(),
 	'urlManagerRules' => [
 		'class'      => 'yii\rest\UrlRule', 
 		'controller' => [
 			'support/v1/feedbacks',
 		],
 		'pluralize'  => false,
-	],    
+	],
 ];
