bd7f4a3dbc3da9d44afe7fe3609fee31a626bbe3
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-01-15 12:09:20 +0700

    commit

--- models/SupportFeedbackSubject.php
+++ models/SupportFeedbackSubject.php
@@ -279,10 +279,13 @@ class SupportFeedbackSubject extends \app\components\ActiveRecord
 	public function beforeValidate() 
 	{
 		if(parent::beforeValidate()) {
-			if($this->isNewRecord)
-				$this->creation_id = !Yii::$app->user->isGuest ? Yii::$app->user->id : '0';
-			else
-				$this->modified_id = !Yii::$app->user->isGuest ? Yii::$app->user->id : null;
+			if($this->isNewRecord) {
+				if($this->creation_id == null)
+					$this->creation_id = !Yii::$app->user->isGuest ? Yii::$app->user->id : '0';
+			} else {
+				if($this->modified_id == null)
+					$this->modified_id = !Yii::$app->user->isGuest ? Yii::$app->user->id : null;
+			}
 		}
 		return true;
 	}
