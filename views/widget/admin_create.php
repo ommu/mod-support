6e8456cd27ea41f916e66a38cc00f54ad76a3b2d
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-02-28 16:32:21 +0700

    commit

--- views/widget/admin_create.php
+++ views/widget/admin_create.php
@@ -26,7 +26,7 @@ $this->params['menu']['content'] = [
 ];
 ?>
 
-<div class="col-md-12 col-sm-12 col-xs-12">
+<div class="col-md-12 col-sm-12 col-12">
 	<div class="x_panel">
 		<div class="x_title">
 			<h2><?php echo Html::encode($this->title); ?></h2>
