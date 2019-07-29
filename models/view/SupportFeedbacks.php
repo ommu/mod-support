723a665d33fdb3606a2d6d217fdf560787a5a17b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-07-29 18:15:07 +0700

    modify getInfo with array column

--- models/view/SupportFeedbacks.php
+++ models/view/SupportFeedbacks.php
@@ -138,11 +138,13 @@ class SupportFeedbacks extends \app\components\ActiveRecord
 	public static function getInfo($id, $column=null)
 	{
 		if($column != null) {
-			$model = self::find()
-				->select([$column])
-				->where(['feedback_id' => $id])
-				->one();
-			return $model->$column;
+			$model = self::find();
+			if(is_array($column))
+				$model->select($column);
+			else
+				$model->select([$column]);
+			$model = $model->where(['feedback_id' => $id])->one();
+			return is_array($column) ? $model : $model->$column;
 			
 		} else {
 			$model = self::findOne($id);
